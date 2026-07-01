17bbd0a5c9423380bdd9580899780fad3e57b834
Orencio Ramirez <ramirez@orencio.es>
2026-07-01 16:43:56 +0200

    agregados autocompletados

--- plugins/completions.zsh
+++ plugins/completions.zsh
@@ -7,7 +7,7 @@
 ###########################################################################
 
 ###########################################################################
-# Comprobación de existencia
+# Comprobación de existencia del plugin
 ###########################################################################
 
 if [[ -f $ZSH_DIR/externos/zsh-completions/zsh-completions.plugin.zsh ]]; then
@@ -15,8 +15,55 @@ if [[ -f $ZSH_DIR/externos/zsh-completions/zsh-completions.plugin.zsh ]]; then
 else
     return
 fi
+
+###########################################################################
+# Comprobación de existencia del complemento Linux
+###########################################################################
+
+[[ -d /usr/share/zsh/functions/Completion/Linux ]] &&
+    fpath=(/usr/share/zsh/functions/Completion/Linux $fpath)
+
+###########################################################################
+# Comprobación de existencia del complemento Debian
+###########################################################################
+
+[[ -d /usr/share/zsh/functions/Completion/Debian ]] &&
+    fpath=(/usr/share/zsh/functions/Completion/Debian $fpath)
+
+###########################################################################
+# Comprobación de existencia del complemento Unix
+###########################################################################
+
+[[ -d /usr/share/zsh/functions/Completion/Unix ]] &&
+    fpath=(/usr/share/zsh/functions/Completion/Unix $fpath)
+
+###########################################################################
+# Cargamos el Plugin
+###########################################################################
 autoload -Uz compinit
 compinit
+
+###########################################################################
+# Visuales
+###########################################################################
+# Menu de seleccion
+zstyle ':completion:*' menu select
+
+# Colores
+autoload -Uz colors && colors
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+# Coincidencia flexible
+zstyle ':completion:*' matcher-list \
+    'm:{a-z}={A-Z}' \
+    'r:|=*' \
+    'l:|=*'
+
+# Agrupar resultados
+zstyle ':completion:*' group-name ''
+
+# Descripciones más legibles
+zstyle ':completion:*:descriptions' format '%F{yellow}-- %d --%f'
 ###########################################################################
 # Fin
 ###########################################################################
